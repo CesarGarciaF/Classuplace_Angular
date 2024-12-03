f7c606ba879fddf6eb6190a9d5ff756ac3e49fb4
lololfo <rodoemma12@gmail.com>
2024-12-03 00:58:27 -0700

    Insersion de cartas en cursos

--- src/app/pages/icons/icons.component.ts
+++ src/app/pages/icons/icons.component.ts
@@ -3,7 +3,18 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'icons-cmp',
     moduleId: module.id,
-    templateUrl: 'icons.component.html'
+    templateUrl: 'icons.component.html',
+    styleUrls: ['./icons.component.css']
 })
 
-export class IconsComponent{}
+export class IconsComponent{
+    isModalOpen = false;
+
+    openModal() {
+      this.isModalOpen = true;
+    }
+  
+    closeModal() {
+      this.isModalOpen = false;
+    }
+}
